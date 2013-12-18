768c44783079fb45fc5aa52b40a648e9061afd4f
Gareth Williams <gareth.williams987@outlook.com>
2013-12-18 14:26:31 +0000

    added 'anonymity'

---
+++ adi.c
@@ -0,0 +1,31 @@
+
+/*
+ * Is Davis Inteligent??
+ */
+
+#include <stdio.h>
+
+void davisIntel();
+
+int main()
+{
+    puts("");
+    
+    davisIntel();
+    
+    puts("");
+    
+    return 0;
+}
+
+
+void davisIntel()
+{
+    int adi = 0;
+    
+    if (adi)
+        puts("Davis is intelligent");
+    else
+        puts("Davis is NOT intelligent");
+    
+}
